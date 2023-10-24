47b26b7999d498f7216079108cd18e4238b2fd85
goodPointP <49491425+goodPointP@users.noreply.github.com>
2023-10-24 23:13:20 +0200

    added websockets

---
+++ server-websockets.py
@@ -0,0 +1,32 @@
+import asyncio
+import websockets
+from websockets.exceptions import ConnectionClosedError
+
+connected_clients = set()
+
+async def echo(websocket, path):
+    connected_clients.add(websocket)
+    try:
+        async for message in websocket:
+            print(f"Received message: {message}")
+            await websocket.send(f"Echo: {message}")
+    except ConnectionClosedError:
+        print("Connection closed unexpectedly")
+    finally:
+        connected_clients.remove(websocket)
+
+async def broadcast(message):
+    if connected_clients:  # Check if there are any clients connected
+        await asyncio.wait([client.send(message) for client in connected_clients])
+
+async def periodic_broadcast():
+    while True:
+        await asyncio.sleep(10)  # Wait for 10 seconds
+        await broadcast("Periodic message from server!")
+
+
+start_server = websockets.serve(echo, "0.0.0.0", 5000)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().create_task(periodic_broadcast())
+asyncio.get_event_loop().run_forever()
+
--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def handle_disconnect():
 
 @socketio.on('message')
 def handle_message(message):
-    print('Received message:', message)
+    print('Received message:', str(message))
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
