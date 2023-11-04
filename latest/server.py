901120c5d2e87c4817b0746eb6ce83bb4a0d20f2
goodPointP <49491425+goodPointP@users.noreply.github.com>
2023-11-04 14:04:27 +0100

    how it should be

--- latest/server.py
+++ latest/server.py
@@ -1,41 +1,66 @@
-# module imports
-import json
 import asyncio
 import websockets
 from websockets.exceptions import ConnectionClosedError
+import random
+import json
 
-# local imports
-from utility_functions import *
-
-# global variables
 connected_clients = set()
 active_sessions = {}
 client_websocket_pairs = {}
 
-# broadcast sends a message to ALL connected clients (disregarding the session_id)
-# send_to_client sends a message to a specific client (identified by the session_id)
+
+async def broadcast(message):
+    if connected_clients:  # Check if there are any clients connected
+        await asyncio.gather(*(client.send(message) for client in connected_clients))
 
 
 async def periodic_broadcast():
-    global connected_clients
-    global active_sessions
     while True:
         print(json.dumps(active_sessions, sort_keys=True, indent=4))
-        await asyncio.sleep(2)  # Wait for 10 seconds
+        await asyncio.sleep(10)  # Wait for 10 seconds
         await broadcast("Periodic message from server!")
         await broadcast(f"There are currently {len(connected_clients)} clients connected across {len(active_sessions)} sessions.")
         await broadcast("Active session info:\n"+str(json.dumps(active_sessions, sort_keys=True, indent=4)))
 
 
-async def serve(websocket, path):
-    global connected_clients
-    global active_sessions
-    global client_websocket_pairs
+async def disconnect_client(websocket, sender_id):
+    # remove client from connected_clients
+    if websocket in connected_clients:
+        connected_clients.remove(websocket)
+        print(f"Client {sender_id} removed from connected_clients")
 
+    # List to store session_ids that need to be deleted
+    sessions_to_delete = []
+
+    for session_id in active_sessions:
+        # Use a list comprehension to remove the client with matching sender_id
+        active_sessions[session_id]["clients"] = [
+            client for client in active_sessions[session_id]["clients"] if client["player_id"] == int(sender_id)]
+
+        if not active_sessions[session_id]["clients"]:
+            sessions_to_delete.append(session_id)
+            print(f"Session {session_id} marked for deletion")
+
+    # Delete the sessions outside the loop
+    for session_id in sessions_to_delete:
+        del active_sessions[session_id]
+        print(f"Session {session_id} deleted")
+
+
+async def send_current_session_state(websocket, session_id):
+    # make packet recognizable by client to be a session state packet
+    package = active_sessions[session_id]
+    package["message_type"] = "session_state"
+    # make a JSON from active_sessions[session_id]
+    json_response = json.dumps(active_sessions[session_id])
+    await websocket.send(json_response)
+
+
+async def serve(websocket, path):
     connected_clients.add(websocket)
-    try:  # try to receive a message from the client
-        async for message in websocket:  # wait for a message from the client
-            try:  # try to parse the message as JSON
+    try:
+        async for message in websocket:
+            try:
                 message = json.loads(message)
             except json.decoder.JSONDecodeError:
                 await websocket.send("Invalid JSON")
@@ -43,9 +68,81 @@ async def serve(websocket, path):
                 continue
 
             sender_id = message["player_id"]
+            # await websocket.send(f"Echo: {message}")
+            # await broadcast(f"Incoming message: {message} from {sender_id}")
 
-            handle_events(message, websocket, sender_id)
+            # create a new session
+            if (message["type"] == "request_create_session"):
+                session_id = str(random.getrandbits(128))
+                sender_id = message["player_id"]
+                map_layout = message["map_layout"]
+                active_sessions[session_id] = {
+                    "session_id": session_id,
+                    "clients": [{"player_id": sender_id,
+                                 "ready": True}],
+                    "state": "waiting",
+                    "action_list": [],
+                    "host": sender_id,
+                    "map_layout": map_layout
+                }
+                client_websocket_pairs[sender_id] = websocket
+                await send_current_session_state(websocket, session_id)
+                await websocket.send(f"Session {session_id} created by player {sender_id}")
 
+            # start an existing session
+            elif (message["type"] == "request_start_session"):
+                session_id = message["session_id"]
+                active_sessions[session_id]["state"] = "running"
+                await broadcast(f"Session {session_id} started")
+                for client in active_sessions[session_id]["clients"]:
+                    # send session_start message to all clients, including the host
+                    receiver_id = client["player_id"]
+                    package = active_sessions[session_id]["state"]
+                    package["message_type"] = "session_start"
+                    # make a JSON from active_sessions[session_id]
+                    json_response = json.dumps(active_sessions[session_id])
+                    await client_websocket_pairs[receiver_id].send(json_response)
+
+            # join an existing session
+            elif (message["type"] == "request_join_session"):
+                sender_id = message["player_id"]
+                session_id = message["session_id"]
+                # check if session exists
+                if session_id not in active_sessions:
+                    await websocket.send(f"Session {session_id} does not exist")
+                    continue
+                # check if player is already in session
+                if sender_id in active_sessions[session_id]["clients"]:
+                    await websocket.send(f"Player {sender_id} is already in session {session_id}")
+                    continue
+                client_to_add = {"player_id": sender_id,
+                                 "ready": True}
+                active_sessions[session_id]["clients"].append(client_to_add)
+                client_websocket_pairs[sender_id] = websocket
+                await send_current_session_state(websocket, session_id)
+                await broadcast(f"Session {session_id} joined by player {sender_id}")
+
+            # send an action to a session
+            elif (message["type"] == "player_action"):
+                # sender_id = message["player_id"]
+                session_id = message["session_id"]
+                move = message["move_data"]
+                move_data = {
+                    "player_id": sender_id, "move_data": move}
+                active_sessions[session_id]["action_list"].append(move_data)
+                for client in active_sessions[session_id]["clients"]:
+                    if client["player_id"] != sender_id:
+                        receiver_id = client["player_id"]
+                        package = active_sessions[session_id]["action_list"][-1]
+                        package["message_type"] = "new_player_action"
+                        # make a JSON from active_sessions[session_id]
+                        json_response = json.dumps(active_sessions[session_id])
+                        await client_websocket_pairs[receiver_id].send(json_response)
+
+            # invalid message type handling
+            else:
+                await websocket.send(f"Unknown message type: {message['type']}")
+                await disconnect_client(websocket, sender_id)
     except ConnectionClosedError as e:
         if e.code == 1006:
             print("Connection closed by client")
